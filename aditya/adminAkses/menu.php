11dbdf178a441c1a9b34ca20196b92621b646d61
adityarikky <aditya.rikky@gmail.com>
2014-11-14 00:19:36 +0700

    bismilah

---
+++ aditya/adminAkses/menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php require_once('../system/cekLogin.php'); ?>
+<html>
+<head>
+	<title>MENU PENITIPAN</title>
+</head>
+<body>
+	<h1>MENU PENITIPAN</h1>
+	<h3>Melihat Semua Data Peminjam</h3>
+	<p><a href="data_peminjam.php">Data Peminjam</a></p>
+	<hr>
+	<h3>Modifikasi Peminjam</h3>
+	<p><a href="form.php">Tambahkan Peminjam</a></p>
+	<hr>
+	<p><a href="../mainMenu.php">Main Menu</a></p>
+</body>
+</html>
