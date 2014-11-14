ca6d2c4d86e2f0f569394f9d5eb9cb6e6dc4cf47
Fatimahtriwulandari <fatimah.wulandari03@gmail.com>
2014-11-14 12:54:02 +0700

    18-04045

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,10 @@
 <?php
-	//konfigurasi database
-	$host = 'localhost';
-	$dbname = 'persewaan';
-	$user = 'root';
-	$pass = '';
 
-	//membuat koneksi ke database
-	$con = mysqli_connect($host, $user, $pass, $dbname);
-	//cek ada kesalahan koneksi atau tidak?
-	if (mysqli_connect_errno()) {
-		echo "Koneksi Error : " . mysqli_connect_error();
-	}
+$host='localhost';
+	$dbname='persewaan';
+	$user='root';
+	$pass='';
+	
+	$con=mysqli_connect($host, $user, $pass, $dbname);
+	
 ?>
