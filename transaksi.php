b1c8c039b87f33b2271bff4d1c298ce4418c5ccc
Fatimahtriwulandari <fatimah.wulandari03@gmail.com>
2014-11-14 12:58:09 +0700

    18-04045

--- transaksi.php
+++ transaksi.php
@@ -1,54 +1,57 @@
 <html>
 <head>
-	<title>Daftar Transaksi</title>
+<title>Daftar Peminjaman</title>
 </head>
 <body>
-<?php 
-
-	//konfigurasi database
-	$host = 'localhost';
-	$dbname = 'persewaan';
-	$user = 'root';
-	$pass = '';
-
-	//membuat koneksi ke database
-	$con = mysqli_connect($host, $user, $pass, $dbname);
-	//cek ada kesalahan koneksi atau tidak?
-	if (mysqli_connect_errno()) {
-		echo "Koneksi Error : " . mysqli_connect_error();
-	}
-
-	//membuat query
-	$query = mysqli_query($con, "SELECT * FROM transaksi");
-
-	//mengambil data hasil query dan menampilkannya
-	
-	echo "<h2>DAFTAR TRANSAKSI</h2>";
+<table><tr>
+	<th><a href="tambah_peminjam.php" align="right">Tambah Data Peminjam</a></th>
+	<th><a href="pengembalian.php" align="right">Pengembalian Buku</a></th>
+	<th><th><a href="logout.php" align="right">Logout</a></th></th>
+</tr></table>
+<?php
+require('koneksi.php');
+session_start();
+if(isset($_SESSION['login']) && ($_SESSION['login'] == 1)){
+	echo "<h2>Daftar Peminjam Buku</h2>";
 	?>
-	<a href="tambahtransaksi.php">Tambah Data Transaksi</a>
-	<table><tr>
-		<th>Id_transaksi</th>
+	<table border="2"><tr>
+		<th>No</th>
 		<th>Judul Buku</th>
-		<th>Tanggal</th>
-		<th>id_penyewa</th>
-		<th colspan="2">Aksi</th>
+		<th>Tanggal Pinjam</th>
+		<th>Id Transaksi</th>
+		<th>Nama Peminjam</th>
+		<th>Nomor Telepon</th>
+		<th>Batas Kembali</th>
+		<th>Tanggal Kembali</th>
+		<th>Denda</th>
 	</tr>
+	
 	<?php
-	$i = 1;
-	while ($data = mysqli_fetch_assoc($query)) {
+	$i=1;
+	$query= mysqli_query($con, "SELECT * From transaksi");
+	while ($data =mysqli_fetch_assoc($query)){
 		echo "<tr>";
 		echo "<td>".$i."</td>";
+		echo "<td>".$data['judul_buku']."</td>";
+		echo "<td>".$data['tanggal_pinjam']."</td>";
 		echo "<td>".$data['id_transaksi']."</td>";
-		echo "<td>".$data['judul']."</td>";
-		echo "<td>".$data['tanggal']."</td>";
-		echo "<td><a href=\"edittransaksi.php?id=".$data['id']."\">Edit</a><td>";
-		echo "<td><a href=\"hapustransaksi.php?id="
-			.$data['id']."\" 
-			onclick=\"alert('Anda Akan Menghapus Data pengguna!')\"> Hapus</a><td>";
-		echo "</tr>";
+		echo "<td>".$data['nama_pelanggan']."</td>";
+		echo "<td>".$data['no_telepon']."</td>";
+		echo "<td>".$data['batas_kembali']."</td>";
+		echo "<td>".$data['tanggal_kembali']."</td>";
+		echo "<td>".$data['denda']."</td>";
 		$i++;
 	}
-	?>
-	</table>
+?>
+</table>
+<tr>
+	<td><a href="index.php">Kembali ke Awal</a></td>
+</tr>
 </body>
 </html>
+
+<?php
+	}else{
+		header('location:login.php');
+	}
+?>
