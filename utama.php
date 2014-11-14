51cfff6dedd4b7dace854159e699ff35b08f3105
Fatimahtriwulandari <fatimah.wulandari03@gmail.com>
2014-11-14 12:59:04 +0700

    18-04045 PERTAMA KALI YANG DI BUKA

---
+++ utama.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Utama</title>
+</head>
+<body>
+
+<?php
+	echo "<h2>Selamat Datang"."</h2>";
+	echo "Pilih menu <a href = \"login.php\">Login Admin</a>";
+	echo " ";
+	echo "ATAU <a href = \"peminjam.php\">Login Peminjam</a>";
+?>
+</body>
+</html>
