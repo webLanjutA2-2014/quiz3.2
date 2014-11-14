20ddd5fc246a17f22c7325bafef3669da4ea66ad
Fatimahtriwulandari <fatimah.wulandari03@gmail.com>
2014-11-14 12:55:45 +0700

    18-04045

---
+++ pencarian.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>PENCARIAN</title>
+</head>
+<body>
+
+<h2 align ="center">Pencarian</h2>
+<form action = "cari.php" method="POST">	
+		<table align="center"; margin="100">
+    <tr>
+      <td align="right">Masukkan Id Transaksi : </td>
+      <td align="left"><input type="text" name="id_transaksi"></td>
+    </tr>
+	<tr>
+    	<td align="right"><input type="submit" value="Cari"></td>
+    </tr>
+	<tr><td><a href="index.php">Kembali</a></td></tr>
+		</form>
+</body>
+</html>
