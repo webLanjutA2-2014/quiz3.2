6e44bd39acaa272f08daca2b9b1f911b4089a458
Fatimahtriwulandari <fatimah.wulandari03@gmail.com>
2014-11-14 12:53:46 +0700

    18-04045

--- index.php
+++ index.php
@@ -8,9 +8,12 @@
 <?php
 session_start();
 	if ((isset($_SESSION['login'])) && ($_SESSION ['login'] == 1)){
-	echo "<h2>Welkomen,".$_SESSION['name']."</h2>";
-	echo "Pilih menu <a href = \"user.php\">User</a>";
+	echo "<h2>Selamat Datang"."</h2>";
+	echo "Pilih menu <a href = \"user.php\">Data User</a>";
+	echo " ";
 	echo "ATAU <a href = \"transaksi.php\">Transaksi</a>";
+	echo " ";
+	echo "ATAU <a href = \"pencarian.php\">Pencarian</a>";
 	}else{
 		header ('location:login.php');
 	}
