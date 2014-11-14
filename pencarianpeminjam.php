80952b5d646dcefcc1ded7bad59699893a6392d6
Fatimahtriwulandari <fatimah.wulandari03@gmail.com>
2014-11-14 12:56:14 +0700

    18-04045

---
+++ pencarianpeminjam.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>PENCARIAN</title>
+</head>
+<body>
+
+<h2 align ="center">Pencarian</h2>
+<form action = "caripeminjam.php" method="POST">	
+		<table align="center"; margin="100">
+    <tr>
+      <td align="right">Masukkan Id Transaksi : </td>
+      <td align="left"><input type="text" name="id_transaksi"></td>
+    </tr>
+	<tr>
+    	<td align="right"><input type="submit" value="Cari"></td>
+    </tr>
+		</form>
+</body>
+</html>
